3f778e7daa5fef1a9ee21e28c1556fa160550e4e
daaviid9 <davidjancus2@gmail.com>
2022-10-20 20:45:36 +0200

    Idle Animation based on mouse

--- Robo Invasion At Our School/Assets/Scripts/Player.cs
+++ Robo Invasion At Our School/Assets/Scripts/Player.cs
@@ -8,13 +8,22 @@ public class Player : MonoBehaviour
     private float moveSpeed;
     [SerializeField]
     private Rigidbody2D rb;
-    
+    [SerializeField]
+    private Animator anim;
+    [SerializeField]
+    private SpriteRenderer player;
+    [SerializeField]
+    private Camera sceneCamera;
+
     private Vector2 moveDirection;
+    private Vector2 mousePosition;
+
 
     // Update is called once per frame
     void Update()
     {
         ProcessInputs();
+        Animate();
     }
 
     private void FixedUpdate()
@@ -27,11 +36,20 @@ public class Player : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
+        // Check for mouse position
         moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
     }
     
     void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    void Animate()
+    {
+        anim.SetFloat("AnimMoveX", mousePosition.x);
+        
+        anim.SetFloat("AnimMoveY", mousePosition.y);
     }
 }
