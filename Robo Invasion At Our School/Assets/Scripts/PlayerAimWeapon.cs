315884a0f7632db83fb7f45710c78e8710ce1259
daaviid9 <davidjancus2@gmail.com>
2022-11-13 21:30:07 +0100

    Aiming

---
+++ Robo Invasion At Our School/Assets/Scripts/PlayerAimWeapon.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimWeapon : MonoBehaviour
+{
+    private Transform aimTransform;
+    [SerializeField]
+    private Animator aimAnim;
+
+    void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+        
+    }
+
+    void Update()
+    {
+        Aiming();
+        Shooting();
+    }
+
+    void Aiming()
+    {
+        Vector3 mousePosition = GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePosition - aimTransform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+
+
+        Vector3 aimLocalScale = Vector3.one;
+        if (angle > 90 || angle < -90)
+        {
+            aimLocalScale.y = -1f;
+        }
+        else
+        {
+            aimLocalScale.y = +1f;
+        }
+
+        aimTransform.localScale = aimLocalScale;
+    }
+
+
+    void Shooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            aimAnim.SetTrigger("Shoot");
+        }
+    }
+
+
+    // Get Mouse Position in World with Z = of
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+    public static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
+
