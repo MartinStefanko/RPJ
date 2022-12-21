ee338334941516039eddf1bdf3379ed24782906a
PoggersMartin <martinste2004@gmail.com>
2022-12-21 21:50:04 +0100

    MeleeEnemy - Movement Mechanic

---
+++ Robo Invasion At Our School/Assets/Scripts/MeleeEnemyAI.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemyAI : MonoBehaviour
+{
+    public float speed;
+    public float checkRadius;
+    public float attackRadius;
+
+    public bool shouldRotate;
+
+    public  LayerMask whatIsPlayer;
+
+    private Transform target;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 movement;
+    public Vector3 dir;
+
+    private bool isInChaseRange;
+    private bool isInAttackRange;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+
+    }
+
+    private void Update()
+    {
+        
+        
+        
+        isInChaseRange = Physics2D.OverlapCircle(transform.position, checkRadius, whatIsPlayer);
+        isInAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius, whatIsPlayer);
+        anim.SetBool("IsRunning", isInChaseRange);
+        Debug.Log(isInChaseRange);
+        Debug.Log(isInAttackRange);
+      
+        dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        dir.Normalize();
+        movement = dir;
+        if (shouldRotate)
+        {
+            anim.SetFloat("AnimMoveX", dir.x);
+            anim.SetFloat("AnimMoveY", dir.y);
+        }
+    }
+
+        private void FixedUpdate()
+        {   
+            if(isInChaseRange && !isInAttackRange)
+            {
+                MoveCharacter(movement);
+            }
+        if (isInAttackRange)
+        {
+            rb.velocity = Vector2.zero;
+
+            }
+        }
+
+    private void MoveCharacter(Vector2 dir)
+    {
+        rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
+
+    }
+
+   }
+  
+
