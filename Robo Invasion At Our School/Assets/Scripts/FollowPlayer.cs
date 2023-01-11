48a4beb849f5291b2f35ac6bb8d1d62fcef0c71f
daaviid9 <davidjancus2@gmail.com>
2023-01-11 15:12:31 +0100

    camera adjustments

--- Robo Invasion At Our School/Assets/Scripts/FollowPlayer.cs
+++ Robo Invasion At Our School/Assets/Scripts/FollowPlayer.cs
@@ -17,14 +17,22 @@ public class FollowPlayer : MonoBehaviour
 
     void FixedUpdate()
     {
-
         Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
-        Vector2 midPoint = (mouseWorldPosition - player.position) / 2.5f;
-        Vector3 midPointV3 = new Vector3(Mathf.Clamp(midPoint.x, -5.80f, 5.80f),
-                                         Mathf.Clamp(midPoint.y, -3.30f, 3.30f), -10);
+        Vector2 midPoint = (mouseWorldPosition - player.position) / 5f;
+        Vector3 midPointV3 = new Vector3(Mathf.Clamp(midPoint.x, -2.15f, 2.15f),
+                                         Mathf.Clamp(midPoint.y, -1.25f, 1.25f), -10);
 
         transform.position = player.position + midPointV3;
-        
+
+        /* 
+        if (midPoint.x < -0.3 || midPoint.x > 0.3 || midPoint.y < -0.3 || midPoint.y > 0.3)
+            transform.position = player.position + midPointV3;
+        else
+           transform.position = new Vector3(player.position.x, player.position.y, -10);
+        */
+
+
+        Debug.Log("midPoint"+midPoint);
     } 
 }
